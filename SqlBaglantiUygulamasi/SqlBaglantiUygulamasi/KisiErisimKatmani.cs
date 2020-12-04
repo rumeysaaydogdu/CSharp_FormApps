440c307ce89ba42f5e4220c5bd7995b63d94ed4f
Rumeysa Aydoğdu <aydogdurumeysa99@hotmail.com>
2020-12-04 04:57:27 +0300

    Apps

---
+++ SqlBaglantiUygulamasi/SqlBaglantiUygulamasi/KisiErisimKatmani.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBaglantiUygulamasi
+{
+    class KisiErisimKatmani
+    {
+        public DataTable GetAll()
+        {
+            SqlConnection baglanti = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog=course; integrated security=true");
+            if(baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+
+            SqlCommand sorgu = new SqlCommand("Select * from Ogrenciler", baglanti);
+            SqlDataReader okuyucu = sorgu.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(okuyucu);
+            okuyucu.Close();
+            baglanti.Close();
+
+            return dataTable;
+        }
+    }
+}
